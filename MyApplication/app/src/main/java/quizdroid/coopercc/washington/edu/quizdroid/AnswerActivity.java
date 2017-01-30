21e2552439fb3b2316b4b72d2cabb233bb01f8c8
Cooper Cain <coopercc@uw.edu>
2017-01-30 11:06:28 -0800

    basic answer activity written out

--- MyApplication/app/src/main/java/quizdroid/coopercc/washington/edu/quizdroid/AnswerActivity.java
+++ MyApplication/app/src/main/java/quizdroid/coopercc/washington/edu/quizdroid/AnswerActivity.java
@@ -2,8 +2,13 @@ package quizdroid.coopercc.washington.edu.quizdroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class AnswerActivity extends Activity {
@@ -23,8 +28,8 @@ public class AnswerActivity extends Activity {
                 intent.putExtra("answer", answer); str
          */
         Intent intent = getIntent();
-        int question = intent.getIntExtra("question", 1);
-        int correct = intent.getIntExtra("correct", 0);
+        final int question = intent.getIntExtra("question", 1);
+        final int correct = intent.getIntExtra("correct", 0);
         String[] answers = intent.getStringArrayExtra("answers");
         String answer = intent.getStringExtra("answer");
 
@@ -32,8 +37,55 @@ public class AnswerActivity extends Activity {
         String newTitle = title.getText().toString() + question;
         title.setText(newTitle);
 
+        RadioGroup rg1 = (RadioGroup) findViewById(R.id.answer_group);
+        RadioButton button;
+        for(int i = 0; i < 4; i++) {
+            button = new RadioButton(this);
+            button.setText(answers[i]);
+            if (answers[i].equals(answer)) {
+                button.setBackgroundColor(Color.parseColor("#ffcc0000"));
+            } else if (i == question) {
+                button.setBackgroundColor(Color.parseColor("#ff669900"));
+            }
+            rg1.addView(button);
+        }
+
+        int newCorrect = correct;
+        //do correct logic here, calculate correct/question
+        if (answer.equals(answers[question])) {
+            newCorrect++;
+        }
+        final int correctPut = newCorrect;
+
+        TextView correctView = (TextView) findViewById(R.id.correct_count);
+        correctView.setText(correctPut + " out of " + question + " correct");
+
+        //change
+
+
+        Button finish = (Button) findViewById(R.id.next);
         if (question == 4) {
-            Button finish = (Button)
+
+            finish.setText("Finish");
+            finish.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(AnswerActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        } else {
+            finish.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(AnswerActivity.this, QuestionsActivity.class);
+                    intent.putExtra("question", question + 1);
+                    intent.putExtra("correct", correctPut);
+
+                    startActivity(intent);
+                }
+            });
         }
 
     }
